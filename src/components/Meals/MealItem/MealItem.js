d72261a893bf6d0e39221304dc1e52c133f0ffe9
Alex <alexbkennedy96@gmail.com>
2021-06-12 15:30:07 -0400

    Added form component and UI input component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,7 @@
 import styles from './MealItem.module.scss'
 
+import MealItemForm from './MealItemForm';
+
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
 
@@ -10,7 +12,7 @@ const MealItem = (props) => {
         <div className={styles.description}>{props.description}</div>
         <div className={styles.price}>{price}</div>
       </div>
-      <div></div>
+      <div><MealItemForm/></div>
     </li>
   );
 };
