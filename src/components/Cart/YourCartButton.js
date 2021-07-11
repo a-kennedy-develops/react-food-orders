edae399a7afc1db739713d7d9d1d5bd31a434c25
Alex <alexbkennedy96@gmail.com>
2021-07-11 14:58:54 -0400

    Adds context for cart and handler component for said context

--- src/components/Cart/YourCartButton.js
+++ src/components/Cart/YourCartButton.js
@@ -1,14 +1,23 @@
+import { useContext } from "react";
+
 import CartIcon from "./CartIcon.js";
+import CartContext from "../../store/cart-context.js";
 import styles from "./YourCartButton.module.scss";
 
 const YourCartButton = (props) => {
+  const cartContext = useContext(CartContext);
+
+  const cartItemsNum = cartContext.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+
   return (
     <button className={styles.button} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span className={styles.text}>Your Cart</span>
-      <span className={styles.badge}>1</span>
+      <span className={styles.badge}>{cartItemsNum}</span>
     </button>
   );
 };
---
+++ src/store/cart-context.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Manage the items (items, num, add/remove)
+const CartContext = React.createContext({
+    items: [],
+    totalAmount: 0,
+    addItem: (item) => {},
+    removeItem: (id) => {}
+});
+
+export default CartContext;
