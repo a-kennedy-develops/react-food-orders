8f3d95aaf9f31016f56e8199c3c88b4d7a1b7149
Alex <alexbkennedy96@gmail.com>
2021-08-16 17:50:29 -0400

    Uses useEffect hook for bump animation on cart button

--- src/components/Cart/YourCartButton.js
+++ src/components/Cart/YourCartButton.js
@@ -1,18 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartIcon from "./CartIcon.js";
 import CartContext from "../../store/cart-context.js";
 import styles from "./YourCartButton.module.scss";
 
 const YourCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartContext = useContext(CartContext);
+
+  const { items } = cartContext;
 
   const cartItemsNum = cartContext.items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (cartContext.items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
