013c5ac159b7982e414c85cb0989cbc7c5f7a4d7
Alex <alexbkennedy96@gmail.com>
2021-12-31 17:56:53 -0500

    Replaces dummy data with Firebase backend; Adds loading text per load

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,46 @@
 import Card from "../UI/Card.js";
 import MealItem from "./MealItem/MealItem.js";
+
+import { useEffect, useState } from "react";
+
 import styles from "./AvailableMeals.module.scss";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Veggie Pizza",
-    description: "Most popular veggie pizza in town!",
-    price: 17.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "A messy, American classic",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "A large, healthy salad loaded with flavor",
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState([]);
+  // const [meals, setMeals] = useState(true);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch('https://react-food-orders-default-rtdb.firebaseio.com/meals.json');
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price
+        });
+      }
+
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals();
+    
+  }, []);
+
+  if (isLoading) {
+    return <section className={styles.MealsLoading}>
+      <p>Loading...</p>
+    </section>
+  }
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
